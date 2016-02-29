ea7c6915f9f0278d50e3d60d7999749373bf8502
Daniel Huisman <daniel@huisman.me>
2016-02-29 12:08:20 +0100

    Added ESLint and Babel config and implemented basic render functionality

--- src/index.js
+++ src/index.js
@@ -1,17 +1,70 @@
+import ReactDOM from 'react-dom/server';
 import Relay from 'relay';
+import {match} from 'react-router';
 import IsomorphicRouter from 'isomorphic-relay-router';
 
-// TODO: install react react-dom react-relay react-router react-router-relay isomorphic-relay isomorphic-relay-router
-// TODO: check version compatibilities for the above packages
-
 export default (options) => {
-    // TODO: validate options
+    // Validate options
+    if (!options.graphqlUrl) {
+        throw new Error('Missing options.graphqlUrl');
+    } else if (!options.routes) {
+        throw new Error('Missing options.routes');
+    } else if (!options.render) {
+        throw new Error('Missing options.render');
+    }
 
     // Initialize middleware
     Relay.injectNetworkLayer(new Relay.DefaultNetworkLayer(options.graphqlUrl));
 
     // Return middleware
-    return async (ctx, next) => {
-        await next();
+    return async (ctx) => {
+        const {redirectLocation, renderProps} = await new Promise((resolve, reject) => {
+            match({
+                routes: options.routes,
+                location: ctx.req.url
+            }, (err, redirectLocation, renderProps) => {
+                if (err) {
+                    return reject(err);
+                }
+                return resolve({redirectLocation, renderProps});
+            });
+        });
+
+        if (redirectLocation) {
+            ctx.redirect(redirectLocation.pathname + redirectLocation.search);
+        } else if (renderProps) {
+            const {data, props} = await IsomorphicRouter.prepareData(renderProps);
+            const reactOutput = ReactDOM.rendertoString(
+                <IsomorphicRouter.RoutingContext {...props} />
+            );
+            const preloadedData = JSON.stringify(data);
+
+            ctx.status = 200;
+            ctx.body = await options.render(reactOutput, preloadedData);
+        } else {
+            ctx.throw(404, 'Not found');
+        }
+
+        // match({
+        //     routes: options.routes,
+        //     location: ctx.req.url
+        // }, (err, redirectLocation, renderProps) => {
+        //     if (err) {
+        //         ctx.throw(500, err.message);
+        //     } else if (redirectLocation) {
+        //         ctx.redirect(redirectLocation.pathname + redirectLocation.search);
+        //     } else if (renderProps) {
+        //         IsomorphicRouter.prepareData(renderProps).then(({data, props}) => {
+        //             const reactOutput = ReactDOM.rendertoString(
+        //                 <IsomorphicRouter.RoutingContext {...props} />
+        //             );
+        //             const preloadedData = JSON.stringify(data);
+        //
+        //             // TODO: render
+        //         }, next);
+        //     } else {
+        //         ctx.throw(404, 'Not found');
+        //     }
+        // });
     };
 };
